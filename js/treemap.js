bdc852a62f998b27744685522f424d4ee0395f9b
MahmoudSalah02 <mahmouds343@gmail.com>
2023-11-26 07:39:51 -0500

    implement onclick behaviour for treemap after user selects a disease

--- js/treemap.js
+++ js/treemap.js
@@ -2,66 +2,86 @@
 
 // Initialize a treemap chart
 function treemap() {
-    // Set the dimensions and margins of the chart
-    let margin = { top: 10, right: 10, bottom: 10, left: 10 },
-        width = 1154 - margin.left - margin.right,
-        height = 1154 - margin.top - margin.bottom;
-  
-    function chart(selector, rawData) {
-      // Process the raw data to extract 'Diseases of heart' data for each age group
-      let data = [];
-      Object.keys(rawData).forEach(ageGroup => {
-        let causeData = rawData[ageGroup][0].Causes.find(cause => cause.Cause === 'Diseases of heart');
-        if (causeData) {
-          data.push({
-            name: ageGroup,
-            value: causeData['Total Deaths']
-          });
-        }
-      });
-  
-      // Compute the layout
-      let root = d3.treemap()
-          .tile(d3.treemapSquarify)
-          .size([width, height])
-          .padding(1)
-          .round(true)
-          (d3.hierarchy({ children: data })
-              .sum(d => d.value));
-  
-      // Append the svg object to the specified selector
-      let svg = d3.select(selector)
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
-  
-      // Add a cell for each leaf of the hierarchy
-      let leaf = svg.selectAll("g")
-        .data(root.leaves())
-        .enter().append("g")
-        .attr("transform", d => `translate(${d.x0},${d.y0})`);
-  
-      // Append a tooltip
-      const format = d3.format(",d");
-      leaf.append("title")
-        .text(d => `${d.parent.data.name}: ${format(d.value)}`);
-  
-      // Append a color rectangle
-      leaf.append("rect")
-        .attr("class", "treemap-rect")
-        .attr("fill", "#ccc")
-        .attr("width", d => d.x1 - d.x0)
-        .attr("height", d => d.y1 - d.y0);
-  
-      // Append text
-      leaf.append("text")
-        .attr("x", 5)
-        .attr("y", 20)
-        .text(d => d.parent.data.name);
+  // Set the dimensions and margins of the chart
+  let margin = { top: 10, right: 10, bottom: 10, left: 10 },
+    width = 1154 - margin.left - margin.right,
+    height = 1154 - margin.top - margin.bottom;
+
+  function processData(rawData, selectedDisease) {
+    console.log("rawData:", rawData); // Add this line
+
+    let data = [];
+    let foundData = false; // Flag to check if data is found for the selected disease
+
+    Object.keys(rawData).forEach(ageGroup => {
+      let causeData = rawData[ageGroup][0].Causes.find(cause => cause.Cause === selectedDisease);
+      if (causeData) {
+        data.push({
+          name: ageGroup,
+          value: causeData['Total Deaths']
+        });
+        foundData = true; // Set flag to true if data is found
+      }
+    });
+
+    // Log if no data is found for the selected disease
+    if (!foundData) {
+      console.log("No data found for the selected disease:", selectedDisease);
     }
-  
-    return chart;
+
+    return data;
   }
-  
+
+  function chart(selector, rawData, selectedDisease = 'Diseases of heart') {
+    d3.select(selector).select("svg").remove(); // Clear the existing treemap
+
+    let data = processData(rawData, selectedDisease);
+
+    // Compute the layout
+    let root = d3.treemap()
+      .tile(d3.treemapSquarify)
+      .size([width, height])
+      .padding(1)
+      .round(true)
+      (d3.hierarchy({ children: data })
+        .sum(d => d.value));
+
+    // Append the svg object to the specified selector
+    let svg = d3.select(selector)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Add a cell for each leaf of the hierarchy
+    let leaf = svg.selectAll("g")
+      .data(root.leaves())
+      .enter().append("g")
+      .attr("transform", d => `translate(${d.x0},${d.y0})`);
+
+    // Append a tooltip
+    const format = d3.format(",d");
+    leaf.append("title")
+      .text(d => `${d.parent.data.name}: ${format(d.value)}`);
+
+    // Append a color rectangle
+    leaf.append("rect")
+      .attr("class", "treemap-rect")
+      .attr("fill", "#ccc")
+      .attr("width", d => d.x1 - d.x0)
+      .attr("height", d => d.y1 - d.y0);
+
+    // Append text
+    leaf.append("text")
+      .attr("x", 5)
+      .attr("y", 20)
+      .text(d => d.parent.data.name);
+  }
+
+  chart.updateData = function (selector, newData, selectedDisease) {
+    chart(selector, newData, selectedDisease);
+  };
+
+  return chart;
+}
--- js/visualization.js
+++ js/visualization.js
@@ -1,10 +1,11 @@
 // Immediately Invoked Function Expression
 ((() => {
-  let pieChartInstance, treeMapChartInstance;
+  let pieChartInstance, treeMapChartInstance, barData, ageData;
 
   // Load and display the bar chart
-  d3.json("data/race.JSON", (error, barData) => {
+  d3.json("data/race.JSON", (error, data) => {
     if (error) throw error;
+    barData = data; // Store the data for later use
 
     let diseaseBarChart = barchart(); // Create the chart function
     diseaseBarChart("#barchart", barData['Total Population'][0].Causes.slice(0, 10));
@@ -12,7 +13,7 @@
     diseaseBarChart.dispatcher().on("diseaseSelected", selectedDisease => {
       console.log("in dispatcher")
       pieChartInstance.updateData("#piechart", barData, selectedDisease);
-      // treeMapChartInstance.updateData("#treemap", ageData, selectedDisease);
+      treeMapChartInstance.updateData("#treemap", ageData, selectedDisease);
     });
   });
 
@@ -26,11 +27,12 @@
   });
 
   // Load and display the initial treemap
-  d3.json("data/age.JSON", (error, treeData) => {
+  d3.json("data/age.JSON", (error, data) => {
     if (error) console.error("Error loading the data:", error);
     else {
+      ageData = data; // Store the data for later use
       treeMapChartInstance = treemap();
-      treeMapChartInstance("#treemap", treeData);
+      treeMapChartInstance("#treemap", ageData);
     }
   });
 })());
